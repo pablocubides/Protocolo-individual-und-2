9004ffac4f0790f19ac0759722de3594a3643669
pablocubides <pcubidesp@unicartagena.edu.co>
2025-09-15 15:47:20 -0500

    ejercios 1,2,3.

--- Taller_7/Taller7eje2/src/taller7eje2/Prueba.java
+++ Taller_7/Taller7eje2/src/taller7eje2/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
     public static void main(String[] args) {
         CBancaria c1 = new CBancaria ("35612", 1000, "Ahorros");
         c1.MostrarDetalles();
-
+        cuenta.mostrarDetalles();
         c1.setSaldo(2000);
         System.out.println("Saldo actualizado: " + c1.getSaldo() + " Pesos");
 
